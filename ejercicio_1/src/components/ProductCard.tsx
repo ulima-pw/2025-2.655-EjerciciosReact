9d88078e4250722c57d890e3bf188ebc5e307fcc
ProfI2104 <ProfI2104@lab.aca>
2025-10-01 11:16:14 -0500

    Comenzando componente ProductCard

--- ejercicio_1/src/components/ProductCard.tsx
+++ ejercicio_1/src/components/ProductCard.tsx
@@ -1,5 +1,10 @@
 const ProductCard = (props : ProductCardProps) => {
-
+    return <div className="card" style="width: 18rem;">
+        <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+        </div>
+    </div>
 }
 
 export type Product = { 
--- ejercicio_1/src/main.tsx
+++ ejercicio_1/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
   </StrictMode>,
