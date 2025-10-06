7208d2ebd0cd33a88211fd59b0161e7c243eadec
ProfI2104 <ProfI2104@lab.aca>
2025-10-06 11:05:55 -0500

    Ejercicio 1 terminado

--- ejercicio_1/src/pages/ProductsPage.tsx
+++ ejercicio_1/src/pages/ProductsPage.tsx
@@ -4,30 +4,31 @@ import ProductGrid from "../components/ProductGrid"
 import Header from "../components/Header"
 import SearchBar from "../components/SearchBar"
 
+const productos : Product[] = [
+    {
+        id : "1",
+        name : "Gaseosa",
+        price : 3.5,
+        category : "Bebidas"
+    },
+    {
+        id : "2",
+        name : "Cafe",
+        price : 2.5,
+        category : "Bebidas"
+    },
+    {
+        id : "5",
+        name : "Galleta",
+        price : 2.5,
+        category : "Golosinas"
+    }
+] 
+
 const ProductsPage = () => {
     const [listaFavoritos, setListaFavoritos] = useState<string[]>([])
     const [textoFiltro, setTextoFiltro] = useState<string>("")
-
-    const productos : Product[] = [
-        {
-            id : "1",
-            name : "Gaseosa",
-            price : 3.5,
-            category : "Bebidas"
-        },
-        {
-            id : "2",
-            name : "Cafe",
-            price : 2.5,
-            category : "Bebidas"
-        },
-        {
-            id : "5",
-            name : "Galleta",
-            price : 2.5,
-            category : "Golosinas"
-        }
-    ] 
+    const [listaProductos, setListaProductos] = useState<Product[]>(productos)
 
     const marcarComoFavorito = (id : string) => {
         if (listaFavoritos.includes(id))
@@ -48,11 +49,21 @@ const ProductsPage = () => {
         <Header totalFavoritos={ listaFavoritos.length } />
         <SearchBar value={ textoFiltro } 
             onChange={ (value : string) => {
+                if (value == "") {
+                    setListaProductos(productos)
+                    setTextoFiltro(value)
+                    return
+                }
+
                 // Filtrar la lista
+                const tempLista = productos.filter( (prod : Product) => {
+                    return prod.name == value
+                } )
+                setListaProductos(tempLista)
                 setTextoFiltro(value)
             } }/>
         <ProductGrid favoritos={ listaFavoritos }
-                products={ productos }
+                products={ listaProductos }
                 onToggleFav={ marcarComoFavorito } />
     </div>
 }
